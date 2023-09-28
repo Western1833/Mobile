ac30411704a87b00c5827da6f1b366396ae055a5
Western1833 <145915895+Western1833@users.noreply.github.com>
2023-09-29 00:04:25 +0300

    Added handlebars, engine, config, layouts

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const config = require('./config.js');
+const config = require('./config/config.js');
+const setupViewEngine = require('./config/handlebars.js');
 
 const app = express();
 
+setupViewEngine(app);
+
 app.get('/', (req, res) => {
-    res.send('Home page');
+    res.render('home');
 });
 
 app.listen(config.PORT, () => console.log(`Server is listening on PORT: ${config.PORT}...`));
